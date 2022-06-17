6e63fd3f12c5f855dc059db8d25f4a926ca5610d
Davinder Singh <dave626429@gmail.com>
2022-06-17 20:33:24 +0530

    adding basic setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
-import { useState } from "react";
+import { Helmet } from "react-helmet";
+import { ThemeProvider } from "styled-components";
 import "./App.css";
+import { GlobalStyle } from "./styles/GlobalStyles";
+import { darkTheme, lightTheme } from "./styles/theme";
+
+export const ThemeContext = React.createContext(null);
 
 function App() {
-  return <div className="App"></div>;
+  const [theme, setTheme] = useState("light");
+  const themestyle = theme === "light" ? lightTheme : darkTheme;
+  return (
+    <ThemeContext.Provider value={{ setTheme, theme }}>
+      <ThemeProvider theme={themestyle}>
+        <GlobalStyle />
+        <Helmet>
+          <title>Sidebar -code focus</title>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,500;0,700;1,400&display=swap"
+            rel="stylesheet"
+          />
+        </Helmet>
+      </ThemeProvider>
+    </ThemeContext.Provider>
+  );
 }
 
 export default App;
